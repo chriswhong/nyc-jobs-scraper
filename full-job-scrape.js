6eea30a77b442817fcf986c4a0dab8941f837028
Chris Whong <chris@whong.org>
2019-04-14 23:18:59 -0400

    parse job content

--- full-job-scrape.js
+++ full-job-scrape.js
@@ -1,6 +1,26 @@
+const cheerio = require('cheerio');
 const puppeteer = require('puppeteer');
 
 const { allAgencyJobIds } = require('./tmp/partial.json');
+const { processCategories } = require('./util/process-categories');
+
+const parseContent = (content) => {
+  const $ = cheerio.load(content);
+  const headers = $('[id^=HRS_SCH_PSTDSC_DESCR\\$]').map(function() {
+    return $(this).text();
+  }).get()
+
+  const bodies = $('[id^=HRS_SCH_PSTDSC_DESCRLONG\\$]').map(function() {
+    return $(this).html();
+  }).get()
+
+  return headers.map((header, i) => {
+    return {
+      header,
+      content: bodies[i],
+    }
+  });
+};
 
 const scrapeJob = async (page) => {
   const data = await page.evaluate(() => {
@@ -8,29 +28,57 @@ const scrapeJob = async (page) => {
       jobId: 'NYC_DRVD_EHIRE_HRS_JOB_OPENING_ID',
       businessTitle: 'HRS_SCH_WRK2_POSTING_TITLE',
       civilServiceTitle: 'NYC_JOBCODE_TBL_DESCR',
-      titleClassification: 'win0divNYC_DRVD_EHIRE_DESCR100lbl',
+      titleClassification: 'NYC_DRVD_EHIRE_DESCR100',
       jobCategories: 'win0divNYC_DRVD_EHIRE_DESCR254A',
       careerLevel: 'win0divNYC_DRVD_EHIRE_DESCR254C',
       workLocation: 'win0divHRS_LOCATION_I_DESCR',
       workUnit: 'HRS_DEPT_I_DESCR',
       numberOfPositions: 'NYC_DRVD_EHIRE_OPENINGS_TARGET',
       titleCodeNumber: 'win0divNYC_DRVD_EHIRE_NYC_TITLE',
+      level: 'NYC_DRVD_EHIRE_NYC_TITLE_ASG_LV',
       proposedSalaryRange: 'NYC_DRVD_EHIRE_DESCR50',
+      postingDate: 'NYC_DRVD_EHIRE_HRS_JO_PST_OPN_DT',
     }
 
     const jobData = {};
     Object.keys(idsToScrape).forEach((key) => {
       jobData[key] = document.getElementById(idsToScrape[key]).innerText;
     });
+
+    jobData.content = document.getElementById('ACE_HRS_SCH_PSTDSC$0').innerHTML;
+
     return jobData;
   });
 
-  return data;
+  // special handling for jobCategories
+  data.jobCategories = processCategories(data.jobCategories);
+
+  // special handling for proposedSalaryRange
+  const [low, high] = data.proposedSalaryRange
+    .match(/(^.*)\(/)[1]
+    .split('-')
+    .map((d) => d.split('$')[1].trim());
+
+  const salaryData = {
+    salaryLow: low,
+    salaryHigh: high,
+    salaryType: data.proposedSalaryRange.match(/\((\w+)\)/)[1],
+  }
+
+  // parse content
+  data.content = parseContent(data.content);
+
+  delete data.proposedSalaryRange;
+
+  return {
+    ...data,
+    ...salaryData,
+  };
 }
 
 const run = async () => {
   const browser = await puppeteer.launch({
-    headless: false,
+    headless: true,
   });
   const page = await browser.newPage();
 
